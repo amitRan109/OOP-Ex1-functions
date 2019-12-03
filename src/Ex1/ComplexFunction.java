e00d3e1c93344b47bb96e1f18cc6f3568f780daa
AmitRan109 <amitshalev109@gmail.com>
2019-12-03 13:35:53 +0200

    better Node constructor

--- src/Ex1/ComplexFunction.java
+++ src/Ex1/ComplexFunction.java
@@ -1,6 +1,6 @@
 package Ex1;
 
-import java.util.Stack;
+//import java.util.Stack;
 
 public class ComplexFunction implements complex_function {
 
@@ -16,11 +16,23 @@ public class ComplexFunction implements complex_function {
 		private Node left;
 		private Operation op;
 		
-		public Node (function _f, Node _left, Node _right, Operation _op) {
+		/*public Node (function _f, Node _left, Node _right, Operation _op) {
 			this. f=_f;
 			this.right=_right; 
 			this.left=_left;
 			this.op=_op;
+		}*/
+		public Node (function _f) {
+			this. f=_f;
+			this.right=null; 
+			this.left=null;
+			this.op=null;
+		}
+		public Node (Operation _op,Node _left, Node _right) {
+			this. f=null;
+			this.right=_right; 
+			this.left=_left;
+			this.op=null;
 		}
 
 		public function getF() {
@@ -61,7 +73,7 @@ public class ComplexFunction implements complex_function {
 	public Node getHead () { return this.head; }
 	
 	public ComplexFunction (function f1) { // build constructor
-		this.head= new Node (null,new Node (f1,null,null,null),null,Operation.None);
+		this.head= new Node (Operation.None,new Node (f1),null);
 		
 	}
 	public ComplexFunction (String op, function f1, function f2) { // build constructor
@@ -69,22 +81,22 @@ public class ComplexFunction implements complex_function {
 		
 		switch (op) {
 		case "plus":
-			this.head= new Node (null,new Node (f1,null,null,null), new Node (f2,null,null,null) ,Operation.Plus);
+			this.head= new Node (Operation.Plus,new Node (f1),null);
 			break;
 		case "mul":
-			this.head= new Node (null,new Node (f1,null,null,null), new Node (f2,null,null,null) ,Operation.Times);
+			this.head= new Node (Operation.Times,new Node (f1),null);
 			break;
 		case "div":
-			this.head= new Node (null,new Node (f1,null,null,null), new Node (f2,null,null,null) ,Operation.Divid);
+			this.head= new Node (Operation.Divid,new Node (f1),null);
 			break;
 		case "max":
-			this.head= new Node (null,new Node (f1,null,null,null), new Node (f2,null,null,null) ,Operation.Max);
+			this.head= new Node (Operation.Max,new Node (f1),null);
 			break;
 		case "min":
-			this.head= new Node (null,new Node (f1,null,null,null), new Node (f2,null,null,null) ,Operation.Min);
+			this.head= new Node (Operation.Min,new Node (f1),null);
 			break;
 		case "comp":
-			this.head= new Node (null,new Node (f1,null,null,null), new Node (f2,null,null,null) ,Operation.Comp);
+			this.head= new Node (Operation.Comp,new Node (f1),null);
 			break;
 		default:
 			throw new RuntimeException("error");
@@ -161,12 +173,12 @@ public class ComplexFunction implements complex_function {
 	public function initFromString(String s) {
 		ComplexFunction ans=new ComplexFunction (new Polynom ("0"));
 		
-		Stack <String> st= new Stack <String> ();
+		/*Stack <String> st= new Stack <String> ();
 		int i=0; int j=0;
 		if (s.charAt(j))
 		while (j<s.length()) {
 			if ()
-		}
+		}*/
 		
 		
 		/*int i=0; int j=0; 
@@ -203,42 +215,42 @@ public class ComplexFunction implements complex_function {
 	public void plus(function f1) {
 		ComplexFunction cf1= new ComplexFunction (f1);
 		Node temp = this.head;
-		this.head= new Node (null,temp,cf1.head,Operation.Plus);
+		this.head= new Node (Operation.Plus, temp, cf1.head);
+		
 	}
 
 	@Override
 	public void mul(function f1) {
 		ComplexFunction cf1= new ComplexFunction (f1);
 		Node temp = this.head;
-		this.head= new Node (null,temp,cf1.head,Operation.Times);
+		this.head= new Node (Operation.Times, temp, cf1.head);
 	}
 
 	@Override
 	public void div(function f1) {
 		ComplexFunction cf1= new ComplexFunction (f1);
 		Node temp = this.head;
-		this.head= new Node (null,temp,cf1.head,Operation.Divid);
+		this.head= new Node (Operation.Divid, temp, cf1.head);
 	}
 
 	@Override
 	public void max(function f1) {
 		ComplexFunction cf1= new ComplexFunction (f1);
 		Node temp = this.head;
-		this.head= new Node (null,temp,cf1.head,Operation.Max);
+		this.head= new Node (Operation.Max, temp, cf1.head);
 	}
 
 	@Override
 	public void min(function f1) {
 		ComplexFunction cf1= new ComplexFunction (f1);
 		Node temp = this.head;
-		this.head= new Node (null,temp,cf1.head,Operation.Min);
-	}
+		this.head= new Node (Operation.Min, temp, cf1.head);	}
 
 	@Override
 	public void comp(function f1) {
 		ComplexFunction cf1= new ComplexFunction (f1);
 		Node temp = this.head;
-		this.head= new Node (null,temp,cf1.head,Operation.Comp);
+		this.head= new Node (Operation.Comp, temp, cf1.head);
 	}
 
 	@Override
